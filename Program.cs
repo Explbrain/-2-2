0dafe3d78f96577fffb26ce1f7669069d83d602d
Людмила <lv-glavbuh@yandex.ru>
2022-11-07 22:55:11 +0300

    Add decision for task10

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿Console.WriteLine("Введите число дня недели");
-int x=Convert.ToInt32 ( Console.ReadLine());
+﻿// Console.WriteLine("Введите число дня недели");
+// int x=Convert.ToInt32 ( Console.ReadLine());
 
-if (x==1) Console.WriteLine("Понедельник. Этот день не выходной");
-else if (x==2) Console.WriteLine("Вторник. Этот день не выходной");
-else if (x==3) Console.WriteLine("Среда. Этот день не выходной");
-else if (x==4) Console.WriteLine("Четверг. Этот день не выходной");
-else if (x==5) Console.WriteLine("Пятница. Этот день не выходной");
-else if (x==6) Console.WriteLine("Суббота. Этот день выходной");
-else if (x==7) Console.WriteLine("Воскесенье. Этот день выходной");
- else Console.WriteLine("Вы ввели не то число");
+// if (x==1) Console.WriteLine("Понедельник. Этот день не выходной");
+// else if (x==2) Console.WriteLine("Вторник. Этот день не выходной");
+// else if (x==3) Console.WriteLine("Среда. Этот день не выходной");
+// else if (x==4) Console.WriteLine("Четверг. Этот день не выходной");
+// else if (x==5) Console.WriteLine("Пятница. Этот день не выходной");
+// else if (x==6) Console.WriteLine("Суббота. Этот день выходной");
+// else if (x==7) Console.WriteLine("Воскесенье. Этот день выходной");
+//  else Console.WriteLine("Вы ввели не то число");
 
+//  Console.WriteLine("Введите трехзначное число");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// int y = x/10;
+// int result = y%10;
+// Console.WriteLine(result);
 
+ Console.WriteLine("Введите натуральное число");
+ int x=Convert.ToInt32 ( Console.ReadLine());
+ int y=1+log10 (double x);
+ Console.WriteLine(y);
